9e78c1dadec114a47d401dedd025c19a4bde370d
machonky <dev@macrobert.com>
2018-03-24 00:06:49 +0800

    Dependency Update, First persistence steps

--- Qapper/Qapper/Qapper.cs
+++ Qapper/Qapper/Qapper.cs
@@ -1,4 +1,5 @@
 ﻿using qSharp;
+using QTools.Schema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace Qapper
 
         public static T[] FromQTable<T>(object o)
         {
-            var properties = _propertyMap[typeof(T)];
+            var properties = MappedTypes[typeof(T)];
 
             if (o is QTable table)
             {
@@ -41,7 +42,7 @@ namespace Qapper
 
         private static string LeadingLowercase(string value) => string.Concat(value[0].ToString().ToLower(), value.Substring(1));
 
-        private static readonly PropertyMap _propertyMap = new PropertyMap(MappingFactory);
+        private static readonly PropertyMap MappedTypes = new PropertyMap(MappingFactory);
 
         private static T[] FromQKeyedTable<T>(Dictionary<string, PropertyInfo> properties, QKeyedTable keyedTable)
         {
@@ -74,6 +75,80 @@ namespace Qapper
                 Array x = (Array)table.Data.GetValue(col);
                 properties[table.Columns[col]].SetValue(result[row], x.GetValue(row));
             }
+        }
+
+        private static bool HasAttribute<T>(this MemberInfo info) where T:Attribute
+        {
+            return info.GetCustomAttribute<T>() != null;
+        }
+
+        public static IQTable ConvertToQTable<T>(IEnumerable<T> dataModels)
+        {
+            var propertiesDict = MappedTypes[typeof(T)];
+
+            var keyedProperties = new Dictionary<string, PropertyInfo>();
+            var unkeyedProperties = new Dictionary<string, PropertyInfo>();
+
+            foreach (var prop in propertiesDict)
+            {
+                if (prop.Value.HasAttribute<KeyAttribute>())
+                {
+                    keyedProperties.Add(prop.Key, prop.Value);
+                }
+                else
+                {
+                    unkeyedProperties.Add(prop.Key, prop.Value);
+                }
+            }
+
+            var keyedTable = keyedProperties.Count == 0 ? null : ConvertToQTable(dataModels, keyedProperties);
+            var unkeyedTable = ConvertToQTable(dataModels, unkeyedProperties);
+
+            if (keyedTable != null)
+            {
+                return new QKeyedTable(keyedTable, unkeyedTable);
+            }
+
+            return unkeyedTable;
+        }
+
+        private static QTable ConvertToQTable<T>(IEnumerable<T> dataModels, Dictionary<string, PropertyInfo> propertiesDict)
+        {
+            var rowCount = dataModels.Count();
+
+            var columns = propertiesDict.Keys.ToArray();
+            var propertiesArray = propertiesDict.Values.ToArray();
+
+            var data = new object[propertiesDict.Count];
+            for (int i = 0; i < data.Length; ++i)
+            {
+                data[i] = Array.CreateInstance(ToQType(propertiesArray[i].PropertyType), rowCount);
+            }
+
+            int rowIndex = 0;
+            var result = new QTable(columns, data);
+            foreach (var dataModel in dataModels)
+            {
+                for (int propIndex = 0; propIndex < propertiesArray.Length; propIndex++)
+                {
+                    var propInfo = propertiesArray[propIndex];
+                    var dataModelValue = propInfo.GetValue(dataModel);
+                    ((Array)data.GetValue(propIndex)).SetValue(ToQObject(dataModelValue, propInfo), rowIndex);
+                }
+
+                rowIndex++;
+            }
+            return result;
+        }
+
+        private static Type ToQType(Type propertyType)
+        {
+            return propertyType;
+        }
+
+        private static object ToQObject(object dataModelValue, PropertyInfo propInfo)
+        {
+            return dataModelValue;
         }
     }
 }
